a612cb3ebada1a2dc0460cec5dcd8fc2db20a893
unknown <sctcad@C422IT06.SCT.local>
2015-01-12 09:10:09 -0500

    Initial Commit -- Project base with openGL included/ready to go.

---
+++ GAME3011_KW_A1/GAME3011_KW_A1/Tile.h
@@ -0,0 +1,19 @@
+//Tiles form the backbone of the game. 
+
+#ifndef _TILE_H_
+#define _TILE_H_
+
+//includes
+
+class Tile 
+{
+
+private:
+protected:
+public:
+
+};
+
+#endif
+
+//KW
---
+++ GAME3011_KW_A1/GAME3011_KW_A1/main.cpp
@@ -0,0 +1,28 @@
+// Advanced Game Programming -- Assignment One  
+// Kayla Wright, 100853637
+// Project Span: 2015.01.12 - 2015.02.01 (~three weeks)
+
+//standard includes.
+#include <iostream>
+
+//openGL includes.
+#include "include\GL\glut.h"
+
+//game-specific includes
+#include "Tile.h"
+
+//function prototypes.
+
+//global variables.
+
+
+//PROGRAM START. 
+int main(int _argc, char* _argv)
+{
+	std::cout << "Test." << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
+
+//KW 
